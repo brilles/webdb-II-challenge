c242cd481903d8115c963391400c57e34aeaa3c1
Brian Illes <brianrilles@gmail.com>
2019-03-26 14:18:55 -0700

    makes port dynamic

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config();
+
 const server = require('./api/server.js');
 
-const port = 3300;
+const port = process.env.PORT || 3300;
 server.listen(port, function() {
   console.log(`\n=== Web API Listening on http://localhost:${port} ===\n`);
 });
