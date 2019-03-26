90e345212b35a44b8b2399ab3bf1cb5bcf8b3054
Brian Illes <brianrilles@gmail.com>
2019-03-26 15:25:23 -0700

    user can see the zoos

--- client/zoos/src/App.js
+++ client/zoos/src/App.js
@@ -3,20 +3,25 @@ import axios from 'axios';
 import './App.css';
 
 export default function App() {
-  const [zoos, setZoos] = useState('');
+  const [zoos, setZoos] = useState([]);
 
   useEffect(() => {
     (async function getData() {
-      const res = await axios.get('https://zoos-app.herokuapp.com/api/zoos');
+      try {
+        const res = await axios.get('https://zoos-app.herokuapp.com/api/zoos');
+        setZoos(res.data);
+      } catch (error) {
+        console.log(error);
+      }
     })();
   }, []);
 
   return (
     <div className="App">
       <h1>Zoos:</h1>
-      {/* {zoos.map(zoo => (
-        <Zoo name={zoo.name}/>
-      ))} */}
+      {zoos.map(zoo => (
+        <h3 key={zoo.id}>{zoo.name}</h3>
+      ))}
     </div>
   );
 }
